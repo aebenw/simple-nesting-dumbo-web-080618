2fa3636b6a8eefbaf5577ebe5509a9cc39714f40
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-20 14:46:45 -0700

    done

--- nested.rb
+++ nested.rb
@@ -16,8 +16,13 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
+
 
 end
+
+
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +42,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+        return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +61,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+        return programmer_hash[:dennis_ritchie][:languages].join
 end
 
 def adding_matz
@@ -80,7 +87,11 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {
+       :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+     }
+programmer_hash
 end
 
 def changing_alan
@@ -100,7 +111,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+ programmer_hash
      
      
 end
@@ -121,5 +133,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+        programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
